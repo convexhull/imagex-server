df0cc11a08453637418255d0e692bb8179cd8230
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-08-04 23:33:48 +0530

    TYPE:FEATURE -> axios config/data/url setup for cv controller

--- components/computer-vision/controller.js
+++ components/computer-vision/controller.js
@@ -4,30 +4,37 @@ const axios = require('axios');
 
 
 const getSimilarImages = async (req, res) => {
-    console.log(req.file);
     const base64Image = req.file.buffer.toString('base64');
-    axios.post('https://api.shutterstock.com/v2/cv/images', {
+    let imageUploadURL = `https://api.shutterstock.com/v2/cv/images`;
+    let imageUploadData = {
         "base64_image": base64Image
-    }, {
+    };
+    let imageUploadConfig = {
         headers: {
             'Authorization': "Bearer v2/QUlPVjJWVUpHd0FRckxVWTQxczFrUjBTY2JxS3pvdjcvMjY3OTYxMTE4L2N1c3RvbWVyLzMvY3JsMWxmR3p2Z1pydXFLR2hXcnFkeVpLRUthWEdMWXRwNkVQVWZRejIzQ29PXzVhSTBsVk14RGxoRWU3QUtEZGUwZVhJejdiTlZQQVJ5aHc4RHZPWXl3LUFxN1YzTU12MjFlMXUwWWZ1VjFSMERZVzZqU3l0RUptRFlCUktTNm41QU55dkRyZ3RkSzVBWUVZVEVZN055TGVCd2dGZEttLVpmd1lFR2tqSzFrdE92dTV5YWhBR094U2czX011N3VoRy1uMHIxbklDVDZlZ2NsOFExLXFZZw"
         }
-    })
-        .then(data => {
-            console.log(data.data);
-            return axios.get(`https://api.shutterstock.com/v2/cv/similar/images?asset_id=${data.data.upload_id}`, {
-                headers: {
-                    'Authorization': "Bearer v2/QUlPVjJWVUpHd0FRckxVWTQxczFrUjBTY2JxS3pvdjcvMjY3OTYxMTE4L2N1c3RvbWVyLzMvY3JsMWxmR3p2Z1pydXFLR2hXcnFkeVpLRUthWEdMWXRwNkVQVWZRejIzQ29PXzVhSTBsVk14RGxoRWU3QUtEZGUwZVhJejdiTlZQQVJ5aHc4RHZPWXl3LUFxN1YzTU12MjFlMXUwWWZ1VjFSMERZVzZqU3l0RUptRFlCUktTNm41QU55dkRyZ3RkSzVBWUVZVEVZN055TGVCd2dGZEttLVpmd1lFR2tqSzFrdE92dTV5YWhBR094U2czX011N3VoRy1uMHIxbklDVDZlZ2NsOFExLXFZZw"
-                }
-            })
-        })
-        .then(d => {
-            console.log(d.data);
-            res.send(d.data);
-        })
-        .catch(e => {
-            console.log(e.message);
-        })
+    }
+    let imageUploadResponse = await axios.post(imageUploadURL, imageUploadData, imageUploadConfig);
+    console.log(imageUploadResponse.data);
+    res.send(imageUploadResponse.data);
+    // axios.post(imageUploadURL, , {
+        
+    // })
+    //     .then(data => {
+    //         console.log(data.data);
+    //         return axios.get(`https://api.shutterstock.com/v2/cv/similar/images?asset_id=${data.data.upload_id}`, {
+    //             headers: {
+    //                 'Authorization': "Bearer v2/QUlPVjJWVUpHd0FRckxVWTQxczFrUjBTY2JxS3pvdjcvMjY3OTYxMTE4L2N1c3RvbWVyLzMvY3JsMWxmR3p2Z1pydXFLR2hXcnFkeVpLRUthWEdMWXRwNkVQVWZRejIzQ29PXzVhSTBsVk14RGxoRWU3QUtEZGUwZVhJejdiTlZQQVJ5aHc4RHZPWXl3LUFxN1YzTU12MjFlMXUwWWZ1VjFSMERZVzZqU3l0RUptRFlCUktTNm41QU55dkRyZ3RkSzVBWUVZVEVZN055TGVCd2dGZEttLVpmd1lFR2tqSzFrdE92dTV5YWhBR094U2czX011N3VoRy1uMHIxbklDVDZlZ2NsOFExLXFZZw"
+    //             }
+    //         })
+    //     })
+    //     .then(d => {
+    //         console.log(d.data);
+    //         res.send(d.data);
+    //     })
+    //     .catch(e => {
+    //         console.log(e.message);
+    //     })
 }   
 
 
