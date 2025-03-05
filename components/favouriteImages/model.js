11e98f6c43225bd3a5af217da87da647c582b707
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-05 16:20:15 +0530

    Formatting changes

--- components/favouriteImages/model.js
+++ components/favouriteImages/model.js
@@ -1,20 +1,17 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-
 const imageSchema = Schema({
-    platform: { type: String}, 
-    pageUrl: { type: String },
-    largeImageUrl: { type: String },
-    mediumImageUrl: { type: String },
-    smallImageUrl: { type: String },
-    downloadUrl: { type: String },
-    imageId: { type: String },
-    createdAt: { type: Date, default: Date.now},
-    aspect: { type: Number }
+  platform: { type: String },
+  pageUrl: { type: String },
+  largeImageUrl: { type: String },
+  mediumImageUrl: { type: String },
+  smallImageUrl: { type: String },
+  downloadUrl: { type: String },
+  imageId: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  aspect: { type: Number },
 });
-
 
 module.exports = mongoose.model("Image", imageSchema);
--- components/favouriteImages/services/index.js
+++ components/favouriteImages/services/index.js
@@ -1,25 +1,24 @@
-const db = require('./db.service');
+const db = require("./db.service");
 
 const saveImage = async (payload, user) => {
-    let imageToSave = {
-        platform: payload.platform, 
-        imageId: payload.imageId,
-        pageUrl: payload.pageUrl,
-        smallImageUrl: payload.smallImageUrl,
-        mediumImageUrl: payload.mediumImageUrl,
-        largeImageUrl: payload.largeImageUrl,
-        downloadUrl: payload.downloadUrl,
-        aspect: payload.aspect
-    };
-    try {
-        let savedImage = await db.insertImage(imageToSave);
-        return savedImage        
-    } catch(e) {    
-        throw e;
-    }
-}
-
+  let imageToSave = {
+    platform: payload.platform,
+    imageId: payload.imageId,
+    pageUrl: payload.pageUrl,
+    smallImageUrl: payload.smallImageUrl,
+    mediumImageUrl: payload.mediumImageUrl,
+    largeImageUrl: payload.largeImageUrl,
+    downloadUrl: payload.downloadUrl,
+    aspect: payload.aspect,
+  };
+  try {
+    let savedImage = await db.insertImage(imageToSave);
+    return savedImage;
+  } catch (e) {
+    throw e;
+  }
+};
 
 module.exports = {
-    saveImage
-}
+  saveImage,
+};
