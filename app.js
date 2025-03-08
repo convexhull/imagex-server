31de30c2ca693c0cd342c04fdfc10d37355cbaa5
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-08 19:05:04 +0530

    Prod cors() fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
       "http://localhost:3000",
       "https://yashpratapsingh.com",
       "http://localhost:3001",
-      "https://imagex-nextjs-frontend.vercel.app/",
+      "https://imagex-nextjs-frontend.vercel.app",
     ],
     credentials: true,
   })
