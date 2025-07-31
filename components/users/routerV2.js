3a7366a1ec69ca6ad620478fb34ce54fb6e1ef98
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-07-31 16:54:42 +0530

    API V2: Modify endpoints for users resource

---
+++ components/users/routerV2.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const multer = require("multer");
+
+const upload = multer();
+
+const userController = require("./controller");
+const { authenticateTokenV2 } = require("../../middlewares/middleware");
+
+const router = express.Router();
+
+router
+  .post("/signup", userController.createNewUser)
+  .post("/login", userController.loginUser)
+  .post("/logout", userController.logoutUser)
+  .post("/refresh", userController.refreshToken)
+  .get("/me", authenticateTokenV2, userController.getOwnAccountInfo)
+  .patch("/update", authenticateTokenV2, userController.updateUser)
+  .patch(
+    "/profile-picture",
+    authenticateTokenV2,
+    upload.single("file"),
+    userController.updateProfilePic
+  )
+  .get(
+    "/favourite-images",
+    authenticateTokenV2,
+    userController.getFavouriteImages
+  )
+  .delete(
+    "/favourite-image",
+    authenticateTokenV2,
+    userController.removeFavouriteImage
+  )
+  .get(
+    "/favourite-image",
+    authenticateTokenV2,
+    userController.getFavouriteImage
+  );
+
+module.exports = router;
--- routes/routesV2.js
+++ routes/routesV2.js
@@ -2,9 +2,11 @@ const express = require("express");
 const unsplashRouter = require("../components/unsplash/routerV2");
 const pixabayRouter = require("../components/pixabay/routerV2");
 const computerVisionRouter = require("../components/computer-vision/routerV2");
+const userRouter = require("../components/users/routerV2");
 
 const router = express.Router();
 
+router.use("/users", userRouter);
 router.use("/unsplash", unsplashRouter);
 router.use("/pixabay", pixabayRouter);
 router.use("/computer-vision", computerVisionRouter);
