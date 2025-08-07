e7997bfc70369cb5e566fdce45f5d2e5d6de56fa
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-08-07 16:27:24 +0530

    DB Schema change: update user model schema

--- components/users/model.js
+++ components/users/model.js
@@ -1,16 +1,39 @@
 const mongoose = require("mongoose");
+const Hashing = require("../../utils/hash");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-  userName: { type: String },
-  email: { type: String },
-  password: { type: String },
-  firstName: { type: String },
+  userName: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: true,
+    minlength: [8, "Password should be atleast 8 characters long"],
+  },
+  passwordConfirm: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: "Password and password confirm don't match",
+    },
+  },
+  firstName: { type: String, required: true },
   lastName: { type: String },
   profilePicUrl: { type: String },
   favouriteImages: [{ type: Schema.Types.ObjectId }],
   bio: { type: String },
+});
+
+UserSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await Hashing.encryptPassword(this.password);
+    this.passwordConfirm = undefined;
+  }
 });
 
 module.exports = mongoose.model("User", UserSchema);
