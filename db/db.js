70efd2edb20220d6103e8fa40ef9b31f2689c613
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-08-09 01:57:20 +0530

    Bump mongoose to v8, clean depracated configs for mongoose.connect

--- db/db.js
+++ db/db.js
@@ -1,27 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const dbURL = process.env.MONGODB_URI || "mongodb://localhost:27017/image-x";
 
-(() =>{ 
-
-    mongoose.connect(dbURL, {useNewUrlParser : true , useUnifiedTopology : true});
-
-    mongoose.connection.on('connected', function(){
-        console.log("Mongoose default connection is open to ", dbURL);
-    });
-
-    mongoose.connection.on('error', function(err){
-        console.log("Mongoose default connection has occured "+err+" error");
-    });
-
-    mongoose.connection.on('disconnected', function(){
-        console.log("Mongoose default connection is disconnected");
-    });
-
-    process.on('SIGINT', function(){
-        mongoose.connection.close(function(){
-            console.log("Mongoose default connection is disconnected due to application termination");
-            process.exit(0)
-        });
-    });
+(async () => {
+  try {
+    await mongoose.connect(dbURL);
+    console.log("Connection to MongoDB server successful");
+  } catch (err) {
+    console.error("Connection to MongoDB server failed");
+    console.log(err);
+  }
 })();
